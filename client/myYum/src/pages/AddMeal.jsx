ac10716f77b09bf20be0b453c55ae8e62e231ea3
Katie Hardy <katiehardy@MacBookPro-2.broadband>
2025-04-11 22:07:07 +0100

    button styling for addMeal page

--- client/myYum/src/pages/AddMeal.jsx
+++ client/myYum/src/pages/AddMeal.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const AddMeal = () => {
-  return <div>AddMeal</div>;
+  return (
+    <div className="parent">
+      <button className="mon-button">Monday</button>
+      <button className="tues-button">Tuesday</button>
+      <button className="wed-button">Wednesday</button>
+      <button className="thurs-button">Thursday</button>
+      <button className="fri-button">Friday</button>
+      <button className="sat-button">Saturday</button>
+      <button className="sun-button">Sunday</button>
+    </div>
+  );
 };
 
 export default AddMeal;
